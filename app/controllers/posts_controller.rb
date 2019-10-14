29cd4136bc02ad9e1d8345273f6ae8ca0b93064c
Nyannsuke01 <shingo37310010ve@gmail.com>
2019-10-14 20:33:17 +0900

    Initialize repository

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id:params[:id])
+    @post.content = params[:content]
+    if @post.save
+    flash[:notice] = "投稿内容を編集しました"
+    redirect_to("/posts/index")
+    else
+    render("posts/edit")
+    redirect_to("/posts/#{@post.id}/edit")
+    end
+  end
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
+
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  get "posts/:id/destroy" => "posts#destroy"
+
+  get "/" => "home#top"
+  get "about" => "home#about"
+end
